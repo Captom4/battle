d8ccf05bb472a3594ae29d96abaf4075e5942cc4
Tom Hird <tombjh@hotmail.co.uk>
2018-10-17 13:51:38 +0000

    Added keeping views clean

--- app.rb
+++ app.rb
@@ -1,11 +1,6 @@
 require 'sinatra'
 
 get '/' do
-  "<div>
-    <img src='https://www.google.co.uk/search?q=avatar+the+last+airbender&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjmnMm4x43eAhUsCMAKHTGrBokQ_AUIDigB&biw=1366&bih=626#imgrc=nV3boIpFlyFamM:'>
-   <div>"
-end
-
-get '/secret' do
-  "This is a secret page"
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
 end
