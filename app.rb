f5b9e7a27328aff98f57c2779a57322b80ae54d8
Tom Hird <tombjh@hotmail.co.uk>
2018-10-17 13:20:31 +0000

    added image to work

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
-require "sinatra"
+require 'sinatra'
 
 get '/' do
-  "Hello World"
+  "<div>
+    <img src='https://www.google.co.uk/search?q=avatar+the+last+airbender&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjmnMm4x43eAhUsCMAKHTGrBokQ_AUIDigB&biw=1366&bih=626#imgrc=nV3boIpFlyFamM:'>
+   <div>"
 end
 
 get '/secret' do
-  'This is a secret page'
+  "This is a secret page"
 end
