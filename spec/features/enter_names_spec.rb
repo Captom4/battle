5422c9d6981969c9f55211d778c9d6abccf88ec9
Tom Hird <tombjh@hotmail.co.uk>
2018-10-18 09:57:55 +0000

    added test to infrastructure file

--- spec/features/enter_names_spec.rb
+++
@@ -1,9 +0,0 @@
-describe 'Enter names' do
-  it 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Dave'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
-    expect(page).to have_content 'Dave vs. Mittens'
-  end
-end
