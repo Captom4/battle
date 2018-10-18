1a92747103045e8e2e677d8826a57560532f8baa
Tom <tombjh@hotmail.co.uk>
2018-10-19 00:27:30 +0100

    tidied up app.rb

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 require 'sinatra/base'
-require 'player'
+require 'lib/player'
 
 class Battle < Sinatra::Base
   enable :sessions
-  
+
   get '/' do
     erb :index
   end
-  
+
   post '/names' do
     $player_1 = Player.new(params[:player_1_name])
     $player_2 = Player.new(params[:player_2_name])
@@ -23,10 +23,9 @@ class Battle < Sinatra::Base
   get '/attack' do
     @player_1_name = $player_1.name
     @player_2_name = $player_2.name
-    @player_1.attack(@player_2)
     erb :attack
   end
 
   # start the server if ruby file executed directly
-  run! if app_file == $0
+  run! if app_file == $PROGRAM_NAME
 end
