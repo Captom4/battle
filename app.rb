5b9839b2ea00fa589341c618408599a97410a643
Tom Hird <tombjh@hotmail.co.uk>
2018-10-18 22:04:42 +0000

    tidied up code

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'player'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -7,23 +8,24 @@ class Battle < Sinatra::Base
     erb :index
   end
   
-post '/names' do
-  $player_1 = Player.new(params[:player_1_name])
-  $player_2 = Player.new(params[:player_2_name])
-  redirect '/play'
-end
+  post '/names' do
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
+    redirect '/play'
+  end
 
-get '/play' do
-  @player_1_name = $player_1.name
-  @player_2_name = $player_2.name
-  erb :play
-end
+  get '/play' do
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
+    erb :play
+  end
 
-get '/attack' do
-  @player_1_name = $player_1.name
-  @player_2_name = $player_2.name
-  erb :attack
-end
+  get '/attack' do
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
+    @player_1.attack(@player_2)
+    erb :attack
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
